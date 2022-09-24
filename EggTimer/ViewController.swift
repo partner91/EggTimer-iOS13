416646d130a0ef3bcb3a73cd6a0d6f535ff67386
fhrsak <hrsak.filip@gmail.com>
2022-09-24 22:41:20 +0200

    Finished egg timer app.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -3,13 +3,56 @@
 //  EggTimer
 //
 //  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Copyright © 2019 The Filip Hrsak. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    let eggTimes = ["Soft":300, "Medium" : 420, "Hard": 720]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    var player : AVAudioPlayer!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        let hardness = sender.currentTitle!
+        titleLabel.text = hardness
+        totalTime = eggTimes[hardness]!
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
 
-
+    }
+    
+    @objc func updateTimer(){
+        if secondsPassed<totalTime{
+            secondsPassed+=1
+            let percentageProgress = Float(secondsPassed) / Float(totalTime)
+            progressBar.progress = percentageProgress
+            
+            
+        } else{
+            timer.invalidate()
+            titleLabel.text = "DONE"
+            playSound()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
+                self.titleLabel.text = "How do you like your eggs?"
+            }
+        }
+        
+        func playSound(){
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+            
+        }
+        
+        
+    }
 }
